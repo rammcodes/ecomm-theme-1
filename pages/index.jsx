decef7c60383a44a89153828ad7ec70abbf49fdf
Ram Maheshwari <ram991011@gmail.com>
2021-03-25 14:54:55 +0530

    backup

--- pages/index.jsx
+++ pages/index.jsx
@@ -579,6 +579,300 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="features">
+        <div className="container">
+          <h2 className="heading heading-section heading-section--cent">
+            <span className="heading-section__main">Our New Products</span>
+          </h2>
+          <div className="features__row">
+            <div className="features__row-item">
+              <div className="feature feature--active">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+            <div className="features__row-item">
+              <div className="feature">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+
+            <div className="features__row-item">
+              <div className="feature">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+
+            <div className="features__row-item">
+              <div className="feature">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+
+            <div className="features__row-item">
+              <div className="feature">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+
+            <div className="features__row-item">
+              <div className="feature">
+                <div className="feature__img-cont">
+                  <img
+                    src="./png/truck.png"
+                    alt="icon"
+                    className="feature__img"
+                  />
+                </div>
+                <h3 className="feature__name">Free Delivery Worldwide</h3>
+                <p className="feature__desc">
+                  At Selential, we focus on Quality and Intergrity. So, always
+                  want our customers to know what they will get.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="blog-preview">
+        <div className="container">
+          <h2 className="heading heading-section heading-section--cent">
+            <span className="heading-section__main">Our New Products</span>
+          </h2>
+          <div className="blog-preview__row">
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="blog-preview__row-item">
+              <div className="single-blog-item">
+                <div className="single-blog-item__img-cont">
+                  <img
+                    src="./png/blog-item-preview.png"
+                    alt="Blog"
+                    className="single-blog-item__img"
+                  />
+                </div>
+                <div className="single-blog-item__content">
+                  <div className="single-blog-item__tags">
+                    <span className="single-blog-item__tag">Wood</span>
+                    <span className="single-blog-item__tag">Wood</span>
+                  </div>
+                  <p className="single-blog-item__info">Mr.James Apr 2018</p>
+                  <h3 className="single-blog-item__title">
+                    Guide to Furniture Shopping
+                  </h3>
+                  <p className="single-blog-item__desc">
+                    At Selential, we focus on Quality and Intergrity. So, always
+                    want our customers to know what they will get.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="cta">
+        <div className="container">
+          <div className="cta__news-ltr">
+            <div className="cta__news-ltr-cont">
+              <h3 className="cta__news-ltr-title">
+                Subscribe to our Newsletter
+              </h3>
+              <p className="cta__news-ltr-desc">
+                At Selential, we focus on Quality and Intergrity. So, always
+                want our customers to know what they will get.
+              </p>
+              <form className="cta__news-ltr-form">
+                <input type="text" className="cta__news-ltr-input" placeholder="Email Address" />
+                <button className="cta__news-ltr-submit">Subscribe</button>
+              </form>
+            </div>
+            <div className="cta__news-ltr-img-cont">
+              <img src="./png/news-ltr-img.png" alt="img" className="cta__news-ltr-img" />
+            </div>
+          </div>
+        </div>
+      </section>
       <footer className="main-footer">
         <div className="main-footer__top">
           <div className="container">

2b3f6046d40830a40fb6bc3757b7e3d82aef4f2b
Ram Maheshwari <ram991011@gmail.com>
2021-03-29 17:01:51 +0530

    backup

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,29 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+const faqs = [
+  {
+    que: `How fast is the shipping?`,
+    ans: `The shipping time varies with different products. We offer Fast
+        Delivery as part of our service, so it won’t take much time to
+        deliver any furniture.`,
+  },
+  {
+    que: `How fast is the shipping?`,
+    ans: `The shipping time varies with different products. We offer Fast
+        Delivery as part of our service, so it won’t take much time to
+        deliver any furniture.`,
+  },
+  {
+    que: `How fast is the shipping?`,
+    ans: `The shipping time varies with different products. We offer Fast
+        Delivery as part of our service, so it won’t take much time to
+        deliver any furniture.`,
+  },
+]
 
 export default function Collection() {
+  const [currFaq, setcurrFaq] = useState(0)
   return (
     <>
       <Head>
@@ -50,61 +73,40 @@ export default function Collection() {
             </span>
           </h2>
           <div className="contact-faq__main">
-            <div className="contact-faq__item">
-              <div className="contact-faq__item-que">
-                How fast is the shipping?
-                <img
-                  src="./png/arrow-up-theme.png"
-                  className="contact-faq__item-que-arrow-open"
-                  alt="arrow"
-                />
-              </div>
-              <div className="contact-faq__item-ans">
-                The shipping time varies with different products. We offer Fast
-                Delivery as part of our service, so it won’t take much time to
-                deliver any furniture.
-              </div>
-            </div>
-            <div className="contact-faq__item">
-              <div className="contact-faq__item-que">
-                How fast is the shipping?
-                {/* <img
-                  src="./png/arrow-up-theme.png"
-                  className="contact-faq__item-que-arrow-open"
-                  alt="arrow"
-                /> */}
-                <img
-                  src="./png/arrow-up.png"
-                  className="contact-faq__item-que-arrow"
-                  alt="arrow"
-                />
-              </div>
-              {/* <div className="contact-faq__item-ans">
-                The shipping time varies with different products. We offer Fast
-                Delivery as part of our service, so it won’t take much time to
-                deliver any furniture.
-              </div> */}
-            </div>
-            <div className="contact-faq__item">
-              <div className="contact-faq__item-que">
-                How fast is the shipping?
-                {/* <img
-                  src="./png/arrow-up-theme.png"
-                  className="contact-faq__item-que-arrow-open"
-                  alt="arrow"
-                /> */}
-                <img
-                  src="./png/arrow-up.png"
-                  className="contact-faq__item-que-arrow"
-                  alt="arrow"
-                />
-              </div>
-              {/* <div className="contact-faq__item-ans">
-                The shipping time varies with different products. We offer Fast
-                Delivery as part of our service, so it won’t take much time to
-                deliver any furniture.
-              </div> */}
-            </div>
+            {faqs.map((item, idx) => {
+              return (
+                <div className="contact-faq__item">
+                  <div
+                    className="contact-faq__item-que"
+                    onClick={() => {
+                      if (currFaq === idx) {
+                        setcurrFaq(null)
+                      } else {
+                        setcurrFaq(idx)
+                      }
+                    }}
+                  >
+                    {item.que}
+                    {currFaq === idx ? (
+                      <img
+                        src="./png/arrow-up-theme.png"
+                        className="contact-faq__item-que-arrow-open"
+                        alt="arrow"
+                      />
+                    ) : (
+                      <img
+                        src="./png/arrow-up.png"
+                        className="contact-faq__item-que-arrow"
+                        alt="arrow"
+                      />
+                    )}
+                  </div>
+                  {currFaq === idx ? (
+                    <div className="contact-faq__item-ans">{item.ans}</div>
+                  ) : null}
+                </div>
+              )
+            })}
           </div>
         </div>
       </section>

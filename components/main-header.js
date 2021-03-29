ad53eec434dfe1c3d68253c17137907c3b0fe90c
Ram Maheshwari <ram991011@gmail.com>
2021-03-29 18:15:43 +0530

    backup

--- components/main-header.js
+++ components/main-header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function MainHeader() {
@@ -70,166 +71,174 @@ export default function MainHeader() {
         </div>
         <nav className="main-header__nav">
           <ul className="main-header__links">
-            <li className="main-header__link">
-              <span className="main-header__link-text">Home</span>
-            </li>
-            <li
-              className="main-header__link main-header__link-dd"
-              onMouseEnter={() => {
-                setshopDropDown(true)
-              }}
-              onMouseLeave={() => {
-                setshopDropDown(false)
-              }}
-            >
-              <span className="main-header__link-text">Shop</span>
-              <img
-                src="./png/arrow-down.png"
-                alt="arrow down"
-                className="main-header__link-icon"
-              />
-              {shopDropDown ? (
-                <div className="main-header__shop-dd-wrapper">
-                  <div className="main-header__shop-dd">
-                    <div className="main-header__shop-dd-row">
-                      <div className="main-header__shop-dd-row-item">
-                        <div className="common-list">
-                          <h4 className="common-list__heading">More Links</h4>
-                          <ul className="common-list__items">
-                            <li className="common-list__item">My account</li>
-                            <li className="common-list__item">
-                              Track your order
-                            </li>
-                            <li className="common-list__item">FAQ</li>
-                            <li className="common-list__item">
-                              Payment Methods
-                            </li>
-                            <li className="common-list__item">
-                              Shipping Guide
-                            </li>
-                          </ul>
+            <Link href="/">
+              <li className="main-header__link">
+                <span className="main-header__link-text">Home</span>
+              </li>
+            </Link>
+            <Link href="/collection">
+              <li
+                className="main-header__link main-header__link-dd"
+                onMouseEnter={() => {
+                  setshopDropDown(true)
+                }}
+                onMouseLeave={() => {
+                  setshopDropDown(false)
+                }}
+              >
+                <span className="main-header__link-text">Shop</span>
+                <img
+                  src="./png/arrow-down.png"
+                  alt="arrow down"
+                  className="main-header__link-icon"
+                />
+                {shopDropDown ? (
+                  <div className="main-header__shop-dd-wrapper">
+                    <div className="main-header__shop-dd">
+                      <div className="main-header__shop-dd-row">
+                        <div className="main-header__shop-dd-row-item">
+                          <div className="common-list">
+                            <h4 className="common-list__heading">More Links</h4>
+                            <ul className="common-list__items">
+                              <li className="common-list__item">My account</li>
+                              <li className="common-list__item">
+                                Track your order
+                              </li>
+                              <li className="common-list__item">FAQ</li>
+                              <li className="common-list__item">
+                                Payment Methods
+                              </li>
+                              <li className="common-list__item">
+                                Shipping Guide
+                              </li>
+                            </ul>
+                          </div>
                         </div>
-                      </div>
-                      <div className="main-header__shop-dd-row-item">
-                        <div className="main-header__shop-dd-new-arrival">
-                          <h4 className="main-header__shop-dd-new-arrival-title">
-                            New arrival
-                          </h4>
-                          <div className="main-header__shop-dd-new-arrival-cont">
-                            <div className="main-header__shop-dd-new-arrival-item">
-                              <h5 className="main-header__shop-dd-new-arrival-item-name">
-                                Minimal Nicki Chair
-                              </h5>
-                              <span className="main-header__shop-dd-new-arrival-item-price">
-                                $250
-                              </span>
-                              <div className="main-header__shop-dd-new-arrival-item-img-cont">
-                                <img
-                                  src="./png/chair-bl.png"
-                                  alt=""
-                                  className="main-header__shop-dd-new-arrival-item-img"
-                                />
+                        <div className="main-header__shop-dd-row-item">
+                          <div className="main-header__shop-dd-new-arrival">
+                            <h4 className="main-header__shop-dd-new-arrival-title">
+                              New arrival
+                            </h4>
+                            <div className="main-header__shop-dd-new-arrival-cont">
+                              <div className="main-header__shop-dd-new-arrival-item">
+                                <h5 className="main-header__shop-dd-new-arrival-item-name">
+                                  Minimal Nicki Chair
+                                </h5>
+                                <span className="main-header__shop-dd-new-arrival-item-price">
+                                  $250
+                                </span>
+                                <div className="main-header__shop-dd-new-arrival-item-img-cont">
+                                  <img
+                                    src="./png/chair-bl.png"
+                                    alt=""
+                                    className="main-header__shop-dd-new-arrival-item-img"
+                                  />
+                                </div>
                               </div>
-                            </div>
-                            <div className="main-header__shop-dd-new-arrival-item">
-                              <h5 className="main-header__shop-dd-new-arrival-item-name">
-                                Minimal Nicki Chair
-                              </h5>
-                              <span className="main-header__shop-dd-new-arrival-item-price">
-                                $250
-                              </span>
-                              <div className="main-header__shop-dd-new-arrival-item-img-cont">
-                                <img
-                                  src="./png/chair-bl.png"
-                                  alt=""
-                                  className="main-header__shop-dd-new-arrival-item-img"
-                                />
+                              <div className="main-header__shop-dd-new-arrival-item">
+                                <h5 className="main-header__shop-dd-new-arrival-item-name">
+                                  Minimal Nicki Chair
+                                </h5>
+                                <span className="main-header__shop-dd-new-arrival-item-price">
+                                  $250
+                                </span>
+                                <div className="main-header__shop-dd-new-arrival-item-img-cont">
+                                  <img
+                                    src="./png/chair-bl.png"
+                                    alt=""
+                                    className="main-header__shop-dd-new-arrival-item-img"
+                                  />
+                                </div>
                               </div>
-                            </div>
-                          </div>{' '}
+                            </div>{' '}
+                          </div>
                         </div>
-                      </div>
-                      <div className="main-header__shop-dd-row-item">
-                        <div className="common-list">
-                          <h4 className="common-list__heading">More Links</h4>
-                          <ul className="common-list__items">
-                            <li className="common-list__item">My account</li>
-                            <li className="common-list__item">
-                              Track your order
-                            </li>
-                            <li className="common-list__item">FAQ</li>
-                            <li className="common-list__item">
-                              Payment Methods
-                            </li>
-                            <li className="common-list__item">
-                              Shipping Guide
-                            </li>
-                          </ul>
+                        <div className="main-header__shop-dd-row-item">
+                          <div className="common-list">
+                            <h4 className="common-list__heading">More Links</h4>
+                            <ul className="common-list__items">
+                              <li className="common-list__item">My account</li>
+                              <li className="common-list__item">
+                                Track your order
+                              </li>
+                              <li className="common-list__item">FAQ</li>
+                              <li className="common-list__item">
+                                Payment Methods
+                              </li>
+                              <li className="common-list__item">
+                                Shipping Guide
+                              </li>
+                            </ul>
+                          </div>
                         </div>
-                      </div>
-                      <div className="main-header__shop-dd-row-item">
-                        <div className="main-header__shop-dd-new-arrival">
-                          <h4 className="main-header__shop-dd-new-arrival-title">
-                            Sale
-                          </h4>
-                          <div className="main-header__shop-dd-new-arrival-cont">
-                            <div className="main-header__shop-dd-new-arrival-item">
-                              <h5 className="main-header__shop-dd-new-arrival-item-name">
-                                Minimal Nicki Chair
-                              </h5>
-                              <span className="main-header__shop-dd-new-arrival-item-price">
-                                $250
-                              </span>
-                              <div className="main-header__shop-dd-new-arrival-item-img-cont">
-                                <img
-                                  src="./png/chair-bl.png"
-                                  alt=""
-                                  className="main-header__shop-dd-new-arrival-item-img"
-                                />
+                        <div className="main-header__shop-dd-row-item">
+                          <div className="main-header__shop-dd-new-arrival">
+                            <h4 className="main-header__shop-dd-new-arrival-title">
+                              Sale
+                            </h4>
+                            <div className="main-header__shop-dd-new-arrival-cont">
+                              <div className="main-header__shop-dd-new-arrival-item">
+                                <h5 className="main-header__shop-dd-new-arrival-item-name">
+                                  Minimal Nicki Chair
+                                </h5>
+                                <span className="main-header__shop-dd-new-arrival-item-price">
+                                  $250
+                                </span>
+                                <div className="main-header__shop-dd-new-arrival-item-img-cont">
+                                  <img
+                                    src="./png/chair-bl.png"
+                                    alt=""
+                                    className="main-header__shop-dd-new-arrival-item-img"
+                                  />
+                                </div>
                               </div>
-                            </div>
-                            <div className="main-header__shop-dd-new-arrival-item">
-                              <h5 className="main-header__shop-dd-new-arrival-item-name">
-                                Minimal Nicki Chair
-                              </h5>
-                              <span className="main-header__shop-dd-new-arrival-item-price">
-                                $250
-                              </span>
-                              <div className="main-header__shop-dd-new-arrival-item-img-cont">
-                                <img
-                                  src="./png/chair-bl.png"
-                                  alt=""
-                                  className="main-header__shop-dd-new-arrival-item-img"
-                                />
+                              <div className="main-header__shop-dd-new-arrival-item">
+                                <h5 className="main-header__shop-dd-new-arrival-item-name">
+                                  Minimal Nicki Chair
+                                </h5>
+                                <span className="main-header__shop-dd-new-arrival-item-price">
+                                  $250
+                                </span>
+                                <div className="main-header__shop-dd-new-arrival-item-img-cont">
+                                  <img
+                                    src="./png/chair-bl.png"
+                                    alt=""
+                                    className="main-header__shop-dd-new-arrival-item-img"
+                                  />
+                                </div>
                               </div>
-                            </div>
-                          </div>{' '}
+                            </div>{' '}
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ) : null}
-            </li>
-            <li
-              className="main-header__link  main-header__link-dd"
-              onMouseEnter={() => {
-                setshopDropDown(true)
-              }}
-              onMouseLeave={() => {
-                setshopDropDown(false)
-              }}
-            >
-              <span className="main-header__link-text">Collection</span>
-              <img
-                src="./png/arrow-down.png"
-                alt="arrow down"
-                className="main-header__link-icon"
-              />
-            </li>
-            <li className="main-header__link">
-              <span className="main-header__link-text">Contact</span>
-            </li>
+                ) : null}
+              </li>
+            </Link>
+            <Link href="/collection">
+              <li
+                className="main-header__link  main-header__link-dd"
+                onMouseEnter={() => {
+                  setshopDropDown(true)
+                }}
+                onMouseLeave={() => {
+                  setshopDropDown(false)
+                }}
+              >
+                <span className="main-header__link-text">Collection</span>
+                <img
+                  src="./png/arrow-down.png"
+                  alt="arrow down"
+                  className="main-header__link-icon"
+                />
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li className="main-header__link">
+                <span className="main-header__link-text">Contact</span>
+              </li>
+            </Link>
           </ul>
         </nav>
         <div className="main-header__actions">
